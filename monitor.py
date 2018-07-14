c50c81790e51226ef2ef171432c98e96c58597aa
Emanuel Vintila <emanuel.vintila@gmail.com>
2018-07-14 10:06:52 +0200

    testing the coverage

--- monitor.py
+++ monitor.py
@@ -2,6 +2,7 @@ import coverage
 import sys
 import runpy
 import traceback
+import os
 
 from translator import Translator
 from mem import Mem
@@ -11,13 +12,22 @@ class Monitor:
   @staticmethod
   def run(target: str, filename: str, args):
       # data_file=None, data_suffix=None, cover_pylib=None, auto_data=False, timid=None, branch=None, config_file=True, source=None, omit=None, include=None, debug=None, concurrency=None
+      # @todo: move this in constructor
+      # @todo: get source from params or cfg file
       data_file = ".coverage"
       data_suffix = None
-      cover_pylib = False # measure also Python code within the interpreter
-      auto_data = True
+      cover_pylib = True # measure also Python code within the interpreter
+      auto_data = False
       timid = False
       branch = True
-      cov = coverage.Coverage(data_file, data_suffix, cover_pylib, auto_data, timid, branch)
+      config_file = False
+      source = None#["sys", "os", "pdfrw", os.path.dirname(target)]
+      omit = None
+      include = "*"
+      debug = None
+      concurrency = None
+      
+      cov = coverage.Coverage(data_file, data_suffix, cover_pylib, auto_data, timid, branch, config_file, source, omit)
       
       crashed = False
       
@@ -47,18 +57,29 @@ class Monitor:
         except:
           cov.stop()
           print ("Caught one!")
-          #traceback.print_exc()
+          traceback.print_exc()
           crashed = True
           
         covData = cov.get_data()
-        arcs = covData.arcs('/home/manu/TUM/sem2/graybox_fuzzing/pyfuzz/sandbox/test_code.py')
-        print (arcs)
+        translator = Translator()
+        for filename in covData.measured_files():
+          print (filename)
+          arcs = covData.arcs(os.path.abspath(filename)) # @todo
+          if len(arcs) < 1:
+            continue
+          if arcs is None:
+            print ("arcs of " + filename + " missing!")
+            return True
+          else:
+            print ("arcs: " + str(arcs))
+              
+          translator.onExecutedPath(filename, arcs)
         #cov.save()
         
-        _, executable_statements, excluded_statements, not_run_statements, missing = cov.analysis2('/home/manu/TUM/sem2/graybox_fuzzing/pyfuzz/sandbox/test_code.py')
+        #_, executable_statements, excluded_statements, not_run_statements, missing = #cov.analysis2('/home/manu/TUM/sem2/graybox_fuzzing/pyfuzz/sandbox/test_code.py')
 
-        print ("executable_statements: ")
-        print (executable_statements)
+        #print ("executable_statements: ")
+        #print (executable_statements)
         #print ("excluded_statements: ")
         #print (excluded_statements)
         #print ("not_run_statements: ")
@@ -66,10 +87,8 @@ class Monitor:
         #print ("missing: ")
         #print (missing)
         
-        translator = Translator()
-        translator.onExecutedPath(target, arcs)
         
         #cov.html_report()
-        #Mem.print()
+        Mem.print()
         
         return not crashed
